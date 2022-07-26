ba2ae93b4ca9499fe5bd76129f6da1d121fa67c8
Korhan Çağın Geboloğlu <kcgebologlu@gmail.com>
2022-07-27 00:55:12 +0300

    getter the device current simulator list added

--- multipleSimulatorLauncher.py
+++ multipleSimulatorLauncher.py
@@ -1,4 +1,5 @@
 import os
+import subprocess
 
 SIMULATOR_LIST_PATH = 'MultipleSimulatorList.txt'
 CONFIGURATION_FILE_PATH = 'ProjectConfiguration.yaml'
@@ -24,3 +25,33 @@ def isSavedSimulatorListUsable(path):
 			return False
 	else:
 		return False
+class Simulator:
+    def __init__(self, rawValue):
+    	splitted = rawValue.split("(")
+    	if len(splitted) == 3:
+    		self.name = splitted[0][:-1]
+    		self.version = splitted[1][:-2]
+    		self.id = splitted[2][:-1]
+    	else:
+    		self.name = splitted[0][:-1]
+    		self.version = splitted[1].split(")")
+    		self.id = splitted[3][:-1]
+
+def simulatorListGetterAndPrint():
+	output = subprocess.check_output("xcrun xctrace list devices", shell=True)
+	lines = output.decode("utf-8").split("\n")
+	#Skip lines until Simulators
+	countForSkippingUntilSimulators = 0
+	for line in lines:
+		countForSkippingUntilSimulators += 1
+		if "== Simulators ==" in line:
+			break
+	lines = lines[countForSkippingUntilSimulators:]
+	counterForSelectingItems = 1
+	collector = []
+	for line in lines:
+		if line != "":
+			print(str(counterForSelectingItems)+ ".) " + line)
+			counterForSelectingItems += 1
+			collector.append(Simulator(line))
+	return collector, counterForSelectingItems
