5a4636f239c41586ddf94caebf8890560a38440e
Korhan Çağın Geboloğlu <kcgebologlu@gmail.com>
2022-07-27 00:53:52 +0300

    checker for saved simulator usage added

--- multipleSimulatorLauncher.py
+++ multipleSimulatorLauncher.py
@@ -1,2 +1,26 @@
+import os
+
 SIMULATOR_LIST_PATH = 'MultipleSimulatorList.txt'
 CONFIGURATION_FILE_PATH = 'ProjectConfiguration.yaml'
+def checkSimulatorListExist(path):
+	if os.path.isfile(path):
+		return True
+	return false
+
+def checkUserInputIsAcceptable(question, firstAnswer, secondAnswer):
+	answer = input(question+ " " + firstAnswer +"/"+ secondAnswer + " ");
+	if answer == firstAnswer:
+		return True
+	elif answer == secondAnswer:
+		return False
+	else:
+		checkUserInputIsAcceptable(question,firstAnswer,secondAnswer)
+
+def isSavedSimulatorListUsable(path):
+	if checkSimulatorListExist(path):
+		if checkUserInputIsAcceptable("Do you want to use existing setup?","y","n"):
+			return True
+		else:
+			return False
+	else:
+		return False
